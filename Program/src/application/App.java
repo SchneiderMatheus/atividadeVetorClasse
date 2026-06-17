7a5881ffd08a8790d24d615b4db3b1fbed813cb6
Matheus <matheuswschneider@gmail.com>
2026-06-17 07:54:27 -0300

    Basic

--- Program/src/application/App.java
+++ Program/src/application/App.java
@@ -3,12 +3,27 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Room;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+
+        for(int i = 0; i<n; i++){
+            System.out.println("Rent #"+(i+1));
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room you want: ");
+            int quartos = sc.nextInt();
+            Room rooms = new Room(quartos, name, email);
+        }
 
 
 
--- Program/src/entities/Room.java
+++ Program/src/entities/Room.java
@@ -1,5 +1,39 @@
 package entities;
 
 public class Room {
+    private int [] quartos = new int[10];
+    private String name;
+    private String email;
+    
+    public Room(int [] quartos, String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.quartos = quartos;
+    }
+
+    public int[] getQuartos() {
+        return quartos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setQuartos(int[] quartos) {
+        this.quartos = quartos;
+    }
+
     
 }
