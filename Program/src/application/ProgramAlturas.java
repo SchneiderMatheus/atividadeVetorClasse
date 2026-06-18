ab9c0d9de59a9d9fc2d0c27442dc7a1843c984fd
Matheus <matheuswschneider@gmail.com>
2026-06-18 14:42:45 -0300

    ......

--- Program/src/application/ProgramAlturas.java
+++ Program/src/application/ProgramAlturas.java
@@ -12,12 +12,13 @@ public class ProgramAlturas {
         
         System.out.println("Digite o numero de pessoas a ser inseridas: ");
         int n = sc.nextInt();
-
+        
         Pessoas [] lista = new Pessoas[n];
 
         for(int i = 0; i<lista.length; i++){
             System.out.println("Digite nome da "+(i+1)+"ª pessoa: ");
             String name = sc.nextLine();
+            sc.nextLine();
             System.out.println("Idade: ");
             int age = sc.nextInt();
             System.out.println("Altura:");
@@ -26,19 +27,26 @@ public class ProgramAlturas {
             lista[i] = new Pessoas(name, age, idade);
         }
         double soma =0;
-        double media= soma/n;
+        double media=0;
         String nomes= " ";
         int contador=0;
+        
         for (int i = 0; i < lista.length; i++) {
             soma += lista[i].getAltura();
         }
+        media = soma/n;
         System.out.println("Altura média: "+media);
+
         for (int i = 0; i < lista.length; i++) {
             if (lista[i].getAge()<16) {
-                nomes += lista[i].getName();
+                nomes = nomes+" "+ lista[i].getName();
                 contador ++;
+                
             }
         }
+        double porcentagem = contador/n *100;
+        System.out.println("Pessoas com menos de 16 anos: "+porcentagem+"%");
+        System.out.println(nomes);
         
         sc.close();
     }
