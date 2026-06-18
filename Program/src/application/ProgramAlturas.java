c0359616e0ba34b6e9412d4928c0847f730aefb5
Matheus <matheuswschneider@gmail.com>
2026-06-17 23:30:10 -0300

    Activities

--- Program/src/application/ProgramAlturas.java
+++ Program/src/application/ProgramAlturas.java
@@ -3,11 +3,28 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Pessoas;
+
 public class ProgramAlturas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
-      
+        
+        System.out.println("Digite o numero de pessoas a ser inseridas: ");
+        int n = sc.nextInt();
+
+        Pessoas [] lista = new Pessoas[n];
+
+        for(int i = 0; i<lista.length; i++){
+            System.out.println("Digite nome da "+(i+1)+"ª pessoa: ");
+            String name = sc.nextLine();
+            System.out.println("Idade: ");
+            int age = sc.nextInt();
+            System.out.println("Altura:");
+            double idade = sc.nextDouble();
+
+            lista[i] = new Pessoas(name, age, idade);
+        }
         
         sc.close();
     }
---
+++ Program/src/entities/Pessoas.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class Pessoas {
+    private String name;
+    private int age;
+    private double altura;
+
+    public Pessoas(String name, int age, double altura) {
+        this.name = name;
+        this.age = age;
+        this.altura = altura;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    
+    
+}
