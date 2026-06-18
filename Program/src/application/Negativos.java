9946cd55c3f5e92f471f511caf405e0bd3c04aa3
Matheus <matheuswschneider@gmail.com>
2026-06-17 23:00:33 -0300

    ....

--- Program/src/application/Negativos.java
+++ Program/src/application/Negativos.java
@@ -8,6 +8,22 @@ public class Negativos {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
+        System.out.print("Quantos numeros voce vai digitar?");
+        int n = sc.nextInt();
+
+        int[] lista = new int[n];
+
+        for(int i =0; i<lista.length; i++){
+            System.out.println("Digite o valor "+(i+1));
+            lista[i] = sc.nextInt();
+        }
+        System.out.println("Números negativos: ");
+        for(int i =0; i<lista.length; i++){
+            if (lista[i]<0) {
+                System.out.println(lista[i]);
+                
+            }
+        }
 
 
         sc.close();
