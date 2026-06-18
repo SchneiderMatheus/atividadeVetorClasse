067a8a2e4131c5601475c5b443244dd2a4990a19
Matheus <matheuswschneider@gmail.com>
2026-06-17 23:10:50 -0300

    .

---
+++ Program/src/application/ProgramAlturas.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramAlturas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+      
+        
+        sc.close();
+    }
+}
